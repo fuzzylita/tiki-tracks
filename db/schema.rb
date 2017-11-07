c33fbe294c23ac907fb76175acbb9df96f1ae3dd
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-11-07 10:25:40 -0800

    actually added email to user table

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171107041839) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
