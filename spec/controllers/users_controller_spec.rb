c48c82ce06a2ce07a3d06da1171d0cf19e4b8ed0
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-11-07 14:32:14 -0800

    added dynamic title, header to application template

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+    describe 'GET #new' do
+      render_views
+      it "returns a 200 OK status" do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+ 
+end
