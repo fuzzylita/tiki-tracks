55feb9aed13883b24e0e041dfda6917aa7a6a56d
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-03 16:53:21 -0800

    refactored user show page to render with js

--- app/assets/javascripts/drinks.js
+++ app/assets/javascripts/drinks.js
@@ -39,10 +39,47 @@ function getDrinks(path) {
 
       $("#drinks_list").append(`
       <li>
-        <strong>${linkTo(drink.name, `/drinks/${drink.id}`)}</strong><br>
+        <strong>${linkTo(drink.name, `${path}/${drink.id}`)}</strong><br>
         Created by ${drink.username} on ${drink.formatted_date()}<br>
         Rating: ${drink.rating}
       </li><br>
+      `)
+    })
+  })
+}
+
+function getDrink(path) {
+  $.getJSON(path, (resp) => {
+    if (resp === null ) {
+      $("#drink_info").append(`
+      <h1>Drink does not exist!</h1>
+      `)
+    }
+
+    let drink = new Drink(resp)
+
+    $("#drink_info").append(`
+      <h1>
+        ${drink.name}!
+      </h1>
+
+      <p><strong>Ingredients: </strong></p>
+      <ul id="ingredients_list">
+      </ul>
+
+      <p><strong>Instructions: </strong></p>
+      <ul>
+        <li>${drink.instructions}</li>
+      </ul>
+      <%= render partial: 'ratings/rate_form', locals: { drink: @drink } %>
+      <br>
+    `)
+
+    let ing = $("#ingredients_list")
+
+    drink.ingredients.forEach(function(el) {
+      ing.append(`
+      <li>${el.name} - ${el.quantity}</li>      
       `)
     })
   })
