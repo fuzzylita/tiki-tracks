f388b6e3c2a98ab56b78c53e69e23a1c46003c75
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-13 00:48:25 -0800

    changed rating field integer > float

--- db/migrate/20171121203718_create_ratings.rb
+++ db/migrate/20171121203718_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration[5.1]
   def change
     create_table :ratings do |t|
-      t.integer :rating
+      t.float :rating
       t.integer :drink_id
       t.integer :user_id
     end
